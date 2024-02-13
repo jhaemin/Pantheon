f13d55bc3d1deaa07e05c737825edd682e9e5e2d
Jang Haemin <io@jhaemin.com>
2024-02-14 00:34:09 +0900

    Add hover and selection features to tree

--- src/tree/tree.tsx
+++ src/tree/tree.tsx
@@ -1,16 +1,41 @@
-import { $currentPage } from '@/atoms'
+import { $currentPage, $hoveredNode, $selectedNodes } from '@/atoms'
 import { keepNodeSelectionAttribute } from '@/data-attributes'
 import { Node } from '@/node-class/node'
 import { PageNode } from '@/node-class/page'
 import { useStore } from '@nanostores/react'
 import { Box, Flex, Text } from '@radix-ui/themes'
-import { Fragment } from 'react'
+import { Fragment, useEffect, useRef } from 'react'
+import styles from './tree.module.scss'
 
 export function Tree() {
+  const ref = useRef<HTMLDivElement>(null!)
   const currentPage = useStore($currentPage)
+
+  useEffect(() => {
+    const unsubscribe = $selectedNodes.subscribe((selectedNodes) => {
+      setTimeout(() => {
+        const elements = ref.current.querySelectorAll(`.${styles.selected}`)
+
+        elements.forEach((element) => {
+          element.classList.remove(styles.selected)
+        })
+
+        const doms = selectedNodes.map((node) => {
+          return document.getElementById(`tree-node-${node.id}`)
+        })
+
+        doms.forEach((dom) => dom?.classList.add(styles.selected))
+      }, 0)
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
 
   return (
     <Box
+      ref={ref}
       p="5"
       style={{
         position: 'fixed',
@@ -39,7 +64,16 @@ export function Tree() {
 function PageLabel({ page }: { page: PageNode }) {
   const pageLabel = useStore(page.$pageLabel)
 
-  return <Text weight="bold">{pageLabel}</Text>
+  return (
+    <Text
+      weight="bold"
+      onMouseDown={() => {
+        $selectedNodes.set([page])
+      }}
+    >
+      {pageLabel}
+    </Text>
+  )
 }
 
 export function Children({ node }: { node: Node }) {
@@ -54,7 +88,21 @@ export function Children({ node }: { node: Node }) {
     <Flex direction="column">
       {node.childrenWithSlots.map((node) => (
         <Fragment key={node.id}>
-          <Box>{node.nodeName}</Box>
+          <Box
+            id={`tree-node-${node.id}`}
+            className={styles.treeNode}
+            onMouseEnter={() => {
+              $hoveredNode.set(node)
+            }}
+            onMouseLeave={() => {
+              $hoveredNode.set(null)
+            }}
+            onMouseDown={() => {
+              $selectedNodes.set([node])
+            }}
+          >
+            {node.nodeName}
+          </Box>
           <Box pl="3">
             <Children node={node} />
           </Box>
