b217924a903b163995320558a3384ea2c4ba3677
Jang Haemin <io@jhaemin.com>
2024-03-07 00:36:36 +0900

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,26 @@ import './global-styles.scss'
 export const metadata: Metadata = {
   title: 'Radix Studio',
   description: 'Radix Studio',
+  icons: [
+    {
+      rel: 'icon',
+      url: '/favicon.png',
+    },
+    {
+      rel: 'icon',
+      url: '/favicon-black.svg',
+    },
+    {
+      media: '(prefers-color-scheme: light)',
+      rel: 'icon',
+      url: '/favicon-black.svg',
+    },
+    {
+      media: '(prefers-color-scheme: dark)',
+      rel: 'icon',
+      url: '/favicon-white.svg',
+    },
+  ],
 }
 
 export default function RootLayout({
