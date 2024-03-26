47c2a7a89352cedcf94bceee4bbaa5a257fcd8a5
Jang Haemin <io@jhaemin.com>
2024-03-27 00:47:16 +0900

    Remove node maps

--- src/infer-node-by-name.ts
+++
@@ -1,7 +0,0 @@
-import { type NodeName } from './node-name'
-import { nodeNameNodeMap } from './node-name-node-map'
-
-export type InferNodeByName<N extends NodeName> =
-  N extends keyof typeof nodeNameNodeMap
-    ? InstanceType<(typeof nodeNameNodeMap)[N]>
-    : never
--- src/node-name.ts
+++
@@ -1,6 +0,0 @@
-import type { NodeName as NodeName_2_0_2 } from '@/__generated__/radix-themes-2.0.2'
-import type { NodeName as NodeName_2_0_3 } from '@/__generated__/radix-themes-2.0.3'
-
-export type NativeNodeName = 'Page' | 'View' | 'Fragment' | 'Text'
-
-export type NodeName = NativeNodeName | NodeName_2_0_2 | NodeName_2_0_3
