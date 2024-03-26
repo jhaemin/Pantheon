e5ca898c9e52cb40ff7c64c0c122b626ecf9ecd5
Jang Haemin <io@jhaemin.com>
2024-03-27 00:46:12 +0900

    Force second argument and handle empty string much better

--- src/serialize-props.ts
+++ src/serialize-props.ts
@@ -1,10 +1,26 @@
+import { Prop } from './node-definition'
+
 /**
  * TODO: get required information and remove props that are falsy and not required
  */
-export function serializeProps(props: Record<string, any>) {
+export function serializeProps(
+  props: Record<string, any>,
+  propsDefinition: Prop[],
+) {
   return Object.entries(props)
     .map(([key, value]) => {
+      const def = propsDefinition.find((def) => def.key === key)
+
+      // If the prop is optional and the value is same as default, remove it.
+      if (def && def.required !== true && def.default === value) {
+        return ''
+      }
+
       if (typeof value === 'string') {
+        if (value === '' && def?.required !== true) {
+          return ''
+        }
+
         return `${key}="${value}"`
       }
 
